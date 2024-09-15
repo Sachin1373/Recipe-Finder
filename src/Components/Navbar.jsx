4cbd60d21dfc946faa186807cba989387cf8b609
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-09-15 23:18:03 +0530

    Updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/Logo.png'
-import '../styles/Navbar.css'; 
+import '../Styles/Navbar.css'; 
 
 const Navbar = ({ user, auth }) => {
   return (
